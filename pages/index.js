898ff9766210f4090e515657a4b6584a5f302031
siddike <absiddikhp@gmail.com>
2022-07-08 00:40:19 +0600

    testimonial section remove

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ import Product from "./../sections/Product/Product";
 import Quality from "./../sections/Quality/Quality";
 import Services from "./../sections/Services/Services";
 import Team from "./../sections/Team/Team";
-import Testimonial from "./../sections/Testimonial/Testimonial";
 import Header from "./../components/Header/Header";
 import Core from "./../sections/Core/Core";
 import Hero from "./../sections/Hero/Hero";
@@ -23,7 +22,6 @@ const Home = () => {
       {/* <Explanation /> */}
       <Pricing />
       <Team />
-      <Testimonial />
       <Footer />
     </div>
   );
